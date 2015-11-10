a87ed0352851d96a990fdaf0f83700dcce5a3629
Yaskovich Dmitry <kadavr95@outlook.com>
2015-11-10 23:18:00 +0300

    -

--- Lab5_hard.cpp
+++ Lab5_hard.cpp
@@ -1,17 +1,34 @@
 #include <iostream.h>
+#include <conio.h>
 #include <string>
 
 
 void main()
 {
 	string s;
-	int n;
+	int n,q=0;
+	bool f;
+	cout<<"Enter numbers:\n";
 	do
 	{
 		getline(cin,s);
-		//cout<<s;
 		n=atoi(s.c_str());
-		cout<<n;
+		f=true;
+		while (n%10>0)
+		{
+			if ((n%10)%2==1)
+			{
+				f=false;
+				break;
+			}
+			n=n/10;
+		}
+		if ((f==true)&&(s.empty()==false))
+		{
+			q++;
+		}
 	}
 	while(s.empty()==false);
+	cout<<"Quanity of even numbers:"<<q;
+	getch();
 }
